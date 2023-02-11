c7f143407010d90633e299888aeca56e0c36d3ea
MatheoArias <teoarco@gmail.com>
2023-02-11 16:42:48 -0500

    Proyecto terminado

--- main.js
+++ main.js
@@ -1,36 +1,52 @@
 const eventDate=new Date('Mar 11 2023')
-const eventMonths=eventDate.getMonth()+1
 
-const date = new Date()
-const currentMonths=date.getMonth()+1
-const currentHours=date.getHours()
-const currentMinutes=date.getMinutes()
-const currentSeconds=date.getSeconds()
+const date = new Date();
+const currentHours=date.getHours();
+const currentMinutes=date.getMinutes();
+const currentSeconds=date.getSeconds();
+const deltaMonths=11+(28-date.getDay());
 
-const deltaMonths=eventMonths-currentMonths
-const deltaDays=(deltaMonths)*28+10
+
+function differenceBetweenDay(dateOne, dateTwo) {
+    var dateOnems = dateOne.getTime();
+    var dateTwoms = dateTwo.getTime();
+    var differencems = dateOnems - dateTwoms;
+    return Math.round(differencems / (1000 * 3600 * 24));
+}
+document.querySelector('#days').innerHTML=differenceBetweenDay(eventDate,date)
+
+function differenceBetweenHour(dateOne, dateTwo) {
+    var dateOnems = dateOne.getTime();
+    var dateTwoms = dateTwo.getTime();
+    var differencems = dateOnems - dateTwoms;
+    return Math.round(differencems / (1000 * 3600));
+}
+const differenceBetweenHours=differenceBetweenHour(eventDate,date)
 
 
 const deltaHours=24-currentHours
 let deltaMinutes=60-currentMinutes
 let deltaSeconds=60-currentSeconds
 
-document.querySelector('#days').innerHTML=deltaDays
-document.querySelector('#Hours').innerHTML=deltaHours
-document.querySelector('#minuts').innerHTML=deltaMinutes
 setInterval(function () {
-    if (deltaSeconds>0){
+    if(differenceBetweenHours<=0){
+        document.querySelector('#Hours').innerHTML=0;
+        document.querySelector('#minuts').innerHTML=0;
+    }
+    else if (deltaSeconds>0){
         deltaSeconds=deltaSeconds-1
-        document.querySelector('#seconds').innerHTML=deltaSeconds
-
+        document.querySelector('#seconds').innerHTML=deltaSeconds;
     }
     else{
         deltaSeconds=60
         deltaMinutes=deltaMinutes-1
-        document.querySelector('#seconds').innerHTML=deltaSeconds
-        document.querySelector('#minuts').innerHTML=deltaMinutes
+        document.querySelector('#seconds').innerHTML=deltaSeconds;
+        document.querySelector('#minuts').innerHTML=deltaMinutes;
     }
 }, 1000);
+document.querySelector('#Hours').innerHTML=deltaHours;
+document.querySelector('#minuts').innerHTML=deltaMinutes;
+
 
 const animation=document.querySelectorAll('.animation')
 window.addEventListener('scroll',() => {
@@ -41,9 +57,28 @@ window.addEventListener('scroll',() => {
             animation[i].style.opacity=1;
         }
     }
-
 })
 
+const contentDatetime=document.querySelector('.content__datetime');
+window.addEventListener('scroll',() => {
+    let scrollTop = document.documentElement.scrollTop;
+    let sectionHeight=contentDatetime.offsetTop;
+        if(scrollTop + 400> sectionHeight){
+            contentDatetime.style.opacity='1'
+            contentDatetime.style.transform='translateX(0px)'
+        }
+    }
+)
 
+const contentLocation=document.querySelector('.content__location');
+window.addEventListener('scroll',() => {
+    let scrollTop = document.documentElement.scrollTop;
+    let sectionHeight=contentLocation.offsetTop;
+        if(scrollTop + 400> sectionHeight){
+            contentLocation.style.opacity='1'
+            contentLocation.style.transform='translateX(0px)'
+        }
+    }
+)
 
 
